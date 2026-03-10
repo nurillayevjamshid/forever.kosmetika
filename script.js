78a20a9395b50e9f864839f90fe168b58f11bf55
nurillayevjamshid <nurillayevjamshid@users.noreply.github.com>
2026-03-10 18:04:31 +0500

    style: Adjust checkout page layout and enhance custom select component by removing unnecessary bullet for "Viloyat" option.

--- script.js
+++ script.js
@@ -180,20 +180,16 @@ function buildCustomSelectOptions(selectEl) {
         optionBtn.setAttribute('role', 'option');
         optionBtn.dataset.value = opt.value;
 
-        let iconHtml = '';
-        if (isViloyat) {
-            iconHtml = `
+        const iconHtml = isViloyat
+            ? `
                 <span class="option-icon">
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
                         <circle cx="12" cy="10" r="3"></circle>
                     </svg>
                 </span>
-                <span class="option-bullet option-bullet-viloyat"></span>
-            `;
-        } else {
-            iconHtml = `<span class="option-bullet"></span>`;
-        }
+            `
+            : `<span class="option-bullet"></span>`;
 
         optionBtn.innerHTML = `
             ${iconHtml}
