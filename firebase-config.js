10052df33412e6d3e88ffee8087964f7fed9d956
nurillayevjamshid <nurillayevjamshid@users.noreply.github.com>
2026-03-25 12:02:56 +0500

    mahsulot qoshishda ozgarish

--- firebase-config.js
+++ firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCcXc0svgxLtPvOYwpNUIzyRJGgajbyR9M",
     authDomain: "foreverkosmetika.firebaseapp.com",
     projectId: "foreverkosmetika",
-    storageBucket: "foreverkosmetika.appspot.com",
+    storageBucket: "foreverkosmetika.firebasestorage.app",
     messagingSenderId: "1013746676566",
     appId: "1:1013746676566:web:0032dfcb13239db2d15cf7",
     measurementId: "G-B71E70JH2Z"
